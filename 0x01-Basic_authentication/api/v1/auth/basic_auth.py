2197527dbe695937abb162f65d3857c387e70141
oladapsy <dapoodedeyi@yahoo.com>
2024-11-13 15:46:47 +0000

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Basic authentication class"""
 from .auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ returns the decoded value of a Base64
+            string base64_authorization_header:
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode_byte = base64.b64decode(base64_authorization_header)
+            return decode_byte.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
