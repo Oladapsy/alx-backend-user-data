80502ffa0100e0eecf2e1e6d4558a6f0c713e5ca
oladapsy <dapoodedeyi@yahoo.com>
2024-11-19 10:41:03 +0000

    test

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,8 +32,12 @@ def authenicate():
     """ Authenticate a request """
     if auth:
         excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']
+                          '/api/v1/forbidden/', '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.session_cookie(request) is None:
+                abort(401)
             if not auth.authorization_header(request):
                 return abort(401)
             request.current_user = auth.current_user(request)
