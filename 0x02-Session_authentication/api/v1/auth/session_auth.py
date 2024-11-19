1f6c36e6e5397bf6996d69c71f9172ae1ea5c528
oladapsy <dapoodedeyi@yahoo.com>
2024-11-19 11:16:22 +0000

    debug

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -29,20 +29,11 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         """ returns a User instance based on a cookie value:"""
-        if request is None:
-            return None
-
-        # Get the session ID from the cookie
         session_id = self.session_cookie(request)
 
         if session_id is None:
             return None
 
-        # Get the user ID based on the session ID
         user_id = self.user_id_for_session_id(session_id)
 
-        if user_id is None:
-            return None
-
-        # Retrieve and return the User instance from the database
         return User.get(user_id)
