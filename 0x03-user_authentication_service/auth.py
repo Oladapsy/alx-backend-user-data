29843bcc5b38f94fa08c4feac393db3e283d3717
oladapsy <dapoodedeyi@yahoo.com>
2024-11-20 18:43:52 +0000

    create Find user by session ID method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,7 +69,7 @@ class Auth:
             return None
 
         try:
-            user = self._db.find_user_by(section_id=section_id)
+            user = self._db.find_user_by(session_id=session_id)
             return user
         except NoResultFound:
             return None
