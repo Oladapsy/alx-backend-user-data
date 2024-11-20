d2815bd13a08d16ec6c132fefade9fa306fecd64
oladapsy <dapoodedeyi@yahoo.com>
2024-11-20 22:34:00 +0000

    user.email

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,7 +66,7 @@ def profile() -> Response:
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    return jsonify({"email": {user.email}}), 200
+    return jsonify({"email": user.email}), 200
 
 
 if __name__ == "__main__":
