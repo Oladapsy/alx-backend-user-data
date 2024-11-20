ecad462663ce8f00e484e1e5ec4571590ddd81a6
oladapsy <dapoodedeyi@yahoo.com>
2024-11-20 23:17:06 +0000

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,12 +95,11 @@ class Auth:
         return token
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """hash the password and update the user’s hashed_password field
-            with the new hashed password and the reset_token field to None
-        """
+        """ hash the password and update the user’s hashed_password field with
+            the new hashed password and the reset_token field to None """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             raise ValueError
-        pwd = _hashed_password(password)
+        pwd = _hash_password(password)
         self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
