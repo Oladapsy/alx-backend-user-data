fcdf6b0c376ff02c1eb5cf2d87f5ad8777e995cd
oladapsy <dapoodedeyi@yahoo.com>
2024-11-20 17:28:12 +0000

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,3 +31,15 @@ class Auth:
             hashed_password = _hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Try locating the user by email. If it exists,
+            check the password with bcrypt.checkpw.
+            If it matches return True. In any other case, return False
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            u_passwd = user.hashed_password
+            return bcrypt.checkpw(password.encode('utf-8'), u_passwd)
+        except NoResultFound:
+            return False
