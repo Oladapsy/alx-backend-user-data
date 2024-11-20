a69ec9afc7d02dedd230c011086be30ffcfeff43
oladapsy <dapoodedeyi@yahoo.com>
2024-11-20 18:42:06 +0000

    the auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,3 +60,16 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """It takes a single session_id string argument and returns
+        the corresponding User or None.
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(section_id=section_id)
+            return user
+        except NoResultFound:
+            return None
