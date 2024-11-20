8a46d3d5cb306936a87af176c5d02840e6f6b9d0
oladapsy <dapoodedeyi@yahoo.com>
2024-11-20 16:39:48 +0000

    test

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """a flask app """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.wrappers import Response
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -11,6 +13,22 @@ app = Flask(__name__)
 def index() -> Response:
     """a single get route"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> Response:
+    """ If the user does not exist, the end-point should register
+        it and respond with the following JSON payload:
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": "<registered email>",
+                       "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
